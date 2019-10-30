74912922bc4776216c97c76965ef8cbd6ad1e5e8
beata <beata@emc2wave.com>
2019-10-30 14:03:59 -0700

    add store action reducer dispatch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+
+
+// STORE -> GLOALIZED STATE
+
+// ACTION INCREMENT
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER
+const cunter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;  
+  }
+}
+
+let store = createStore(cunter);
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()))
+
+// DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
