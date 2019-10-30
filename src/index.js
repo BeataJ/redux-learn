c4b1594627f3e4ee9efbd4db1e59513825df3299
beata <beata@emc2wave.com>
2019-10-30 15:35:22 -0700

    add Provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
@@ -45,7 +46,11 @@ const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 // store.dispatch(decrement());
 // store.dispatch(decrement());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
