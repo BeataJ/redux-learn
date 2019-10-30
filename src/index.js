ed5caef305eddace9f3732473686305760a88f0c
beata <beata@emc2wave.com>
2019-10-30 14:11:16 -0700

    comment redux example

--- src/index.js
+++ src/index.js
@@ -3,44 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 
 
 
-// STORE -> GLOALIZED STATE
+// // STORE -> GLOALIZED STATE
 
-// ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
+// // ACTION INCREMENT
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
 
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
 
-// REDUCER
-const cunter = (state = 0, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;  
-  }
-}
+// // REDUCER
+// const cunter = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;  
+//   }
+// }
 
-let store = createStore(cunter);
+// let store = createStore(cunter);
 
-// Display it in the console
-store.subscribe(() => console.log(store.getState()))
+// // Display it in the console
+// store.subscribe(() => console.log(store.getState()))
 
-// DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+// // DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
